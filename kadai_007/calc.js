cae51f9235c53b4bdbaf6b31b9bef513aa15dc23
Maroron1120 <momimaron1027@gmail.com>
2025-04-07 20:05:40 +0900

    課題をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,23 @@
+num = Math.floor(Math.random() * 100);
+console.log(num);
+num3=num % 3 == 0
+num5=num % 5 == 0
+console.log(num3);
+console.log(num5);
+
+if (num3 && num5) {
+  console.log("3と5の倍数です");
+}
+
+else if (num5) {
+  console.log("5の倍数です");
+}
+else if  (num3) {
+  console.log("3の倍数です");
+}
+else {
+  console.log("num");
+}
+num % 3 == 0
+num % 5 == 0
+
